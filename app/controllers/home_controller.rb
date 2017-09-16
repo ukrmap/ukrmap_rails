0f5f5b557c7ca0801c0612c5ff79efe004c40f05
Vitalii Povod <31216008+povodok@users.noreply.github.com>
2017-09-16 11:34:19 +0300

    Save queries into Term table (#10)
    * Add To Term
    
    * remove Entry.search from Term
    
    * remove @terms
    
    * fix Entry.search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     if params[:q]
       @entries = Entry.search(params[:q])
+      Term.log_query(params[:q], @entries.count)
       render :search
     else
       render :index
